0150ecc3bd426b7def16ec54de3911dc2c11b273
Kasper Kouhia <60173278+kasperkouhia@users.noreply.github.com>
2025-12-16 13:47:16 +0200

    Change Person to PersonCard

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,4 +1,15 @@
-export default function Person({ name, title, salary, phone, email, animal }) {
+export default function PersonCard({
+  name,
+  title,
+  salary,
+  phone,
+  email,
+  animal,
+  startDate,
+  location,
+  department,
+  skills,
+}) {
   return (
     <div className="border-1">
       <h2 className="border-b-1 p-4 font-bold">{name}</h2>
@@ -8,6 +19,10 @@ export default function Person({ name, title, salary, phone, email, animal }) {
         <li>Phone number: {phone}</li>
         <li>Email: {email}</li>
         <li>Favorite animal: {animal}</li>
+        <li>Start date: {startDate}</li>
+        <li>Location: {location}</li>
+        <li>Department: {department}</li>
+        <li>Skills: {skills.join(", ")}</li>
       </ul>
     </div>
   );
